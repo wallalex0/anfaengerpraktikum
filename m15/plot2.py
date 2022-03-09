0a7245f55e4335e3ab3275b95902a7f1fe7625ba
wallalex <alexander@jwallrodt.de>
2022-03-09 17:33:43 +0100

    m15 add plot + update

--- m15/plot2.py
+++ m15/plot2.py
@@ -4,23 +4,27 @@ import math
 
 x = []
 y = []
+
 x_error = []
 y_error = []
 
-x_unit = "°"
-y_unit = "s"
+x_data_unit = "m"
+y_data_unit = "kg"
 
-name = "schwingungsdauer-winkel"
+x_graph_unit = "m"
+y_graph_unit = "N"
+
+name = "gewicht-auslenkung"
 
 messwerte = False
 
 with open(name, "r", encoding="utf-8") as file:
     for line in file.readlines():
         temp = line.split(";")
-        x.append((math.sin(math.radians(float(temp[0].strip("\n").strip(x_unit))) / 2)) ** 2)
-        y.append(float(temp[2].strip("\n").strip(y_unit)))
-        x_error.append(float(temp[1].strip("\n").strip(x_unit)))
-        y_error.append(float(temp[3].strip("\n").strip(y_unit)))
+        x.append(float(temp[0].strip("\n").strip(x_data_unit)) - 0.15)
+        y.append(float(temp[2].strip("\n").strip(y_data_unit)) * 9.81)
+        x_error.append(float(temp[1].strip("\n").strip(x_data_unit)))
+        y_error.append(float(temp[3].strip("\n").strip(y_data_unit)) * 9.81)
 
 print(x, y)
 print(x_error, y_error)
@@ -34,20 +38,21 @@ axis = plot.axes()
 
 axis.scatter(x, y, label="Datenpunkte")
 
-axis.set_xlabel('sin²(Φ/2)')
-axis.set_ylabel('T(sin²(Φ/2)) in s')
+axis.set_xlabel('d in m')
+axis.set_ylabel('F(d) in N')
 
 # axis.set_xlim(0, 15000)
 # axis.set_ylim(0, 120)
 
 linear_model = np.polyfit(x, y, 1)
-print(linear_model)
+print(str(linear_model))
 linear_model_fn = np.poly1d(linear_model)
 print(linear_model_fn)
 x_s_step = (x[-1] - x[0]) / 100
 x_s = np.arange(x[0] - x_s_step * 10, x[-1] + x_s_step * 20, x_s_step)
+
 if not messwerte:
-    plot.plot(x_s, linear_model_fn(x_s), color="green", label=f"Steigung: {str(round(float(linear_model[0]), 4))} s\nY-Abschnitt: {str(round(float(linear_model[1]), 4))} s")
+    plot.plot(x_s, linear_model_fn(x_s), color="green", label=f"Steigung: {str(round(float(linear_model[0]), 4))} kg/s²\nY-Abschnitt: {str(round(float(linear_model[1]), 4))} N")
 
 plot.errorbar(x, y, xerr=x_error, yerr=y_error, fmt='o', elinewidth=2)
 
